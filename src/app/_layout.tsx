5fc2c9864ee85a2bc8f821efee75d93d8677f28b
kebsixx <rijikanw@gmail.com>
2024-11-16 20:08:41 +0700

    add react query

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,33 +1,36 @@
 import { Stack } from "expo-router";
 import { ToastProvider } from "react-native-toast-notifications";
 import AuthProvider from "../providers/auth-provider";
+import { QueryProvider } from "../providers/query-provider";
 
 export default function RootLayout() {
   return (
     <ToastProvider>
       <AuthProvider>
-        <Stack>
-          <Stack.Screen
-            name="(shop)"
-            options={{ headerShown: false, title: "Shop" }}
-          />
-          <Stack.Screen
-            name="categories"
-            options={{ headerShown: false, title: "Categories" }}
-          />
-          <Stack.Screen
-            name="product"
-            options={{ headerShown: false, title: "Product" }}
-          />
-          <Stack.Screen
-            name="cart"
-            options={{ presentation: "modal", title: "Shopping Cart" }}
-          />
-          <Stack.Screen
-            name="auth"
-            options={{ headerShown: false, title: "Auth" }}
-          />
-        </Stack>
+        <QueryProvider>
+          <Stack>
+            <Stack.Screen
+              name="(shop)"
+              options={{ headerShown: false, title: "Shop" }}
+            />
+            <Stack.Screen
+              name="categories"
+              options={{ headerShown: false, title: "Categories" }}
+            />
+            <Stack.Screen
+              name="product"
+              options={{ headerShown: false, title: "Product" }}
+            />
+            <Stack.Screen
+              name="cart"
+              options={{ presentation: "modal", title: "Shopping Cart" }}
+            />
+            <Stack.Screen
+              name="auth"
+              options={{ headerShown: false, title: "Auth" }}
+            />
+          </Stack>
+        </QueryProvider>
       </AuthProvider>
     </ToastProvider>
   );
