03d49c66b326cec49e6f61afbd4d912711d65f4e
kebsixx <rijikanw@gmail.com>
2024-12-01 15:01:13 +0700

    order update real-time subscription

--- src/app/(shop)/orders/_layout.tsx
+++ src/app/(shop)/orders/_layout.tsx
@@ -1,6 +1,9 @@
 import { Stack } from "expo-router";
+import { useOrderUpdateSubscription } from "../../../api/subscription";
 
 export default function OrdersLayout() {
+  useOrderUpdateSubscription();
+
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
