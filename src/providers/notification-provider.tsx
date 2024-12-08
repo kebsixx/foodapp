176665859b2e55957576e7b768412df8da164c6e
kebsixx <rijikanw@gmail.com>
2024-12-08 18:38:00 +0700

    setup real time notification for order

--- src/providers/notification-provider.tsx
+++ src/providers/notification-provider.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useRef, PropsWithChildren } from "react";
 import * as Notifications from "expo-notifications";
 import registerForPushNotificationsAsync from "../lib/notifications";
+import { supabase } from "../lib/supabase";
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -18,9 +19,29 @@ const NotificationsProvider = ({ children }: PropsWithChildren) => {
   const notificationListener = useRef<Notifications.Subscription>();
   const responseListener = useRef<Notifications.Subscription>();
 
+  const saveUserPushNotificationToken = async (token: string) => {
+    if (!token.length) return;
+
+    const {
+      data: { session },
+    } = await supabase.auth.getSession();
+
+    if (!session) return;
+
+    await supabase
+      .from("users")
+      .update({
+        expo_notification_token: token,
+      })
+      .eq("id", session.user.id);
+  };
+
   useEffect(() => {
     registerForPushNotificationsAsync()
-      .then((token) => setExpoPushToken(token ?? ""))
+      .then((token) => {
+        setExpoPushToken(token ?? "");
+        saveUserPushNotificationToken(token ?? "");
+      })
       .catch((error: any) => setExpoPushToken(`${error}`));
 
     notificationListener.current =
