8cdaf1969ff6754d158d2fade4ced53864705315
kebsixx <rijikanw@gmail.com>
2024-12-08 11:54:24 +0700

    notification setup

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,34 +3,37 @@ import { ToastProvider } from "react-native-toast-notifications";
 import AuthProvider from "../providers/auth-provider";
 import { QueryProvider } from "../providers/query-provider";
 import { StripeProvider } from "@stripe/stripe-react-native";
+import NotificationProvider from "../providers/notification-provider";
 
 export default function RootLayout() {
   return (
     <ToastProvider>
       <AuthProvider>
         <QueryProvider>
-          <Stack>
-            <Stack.Screen
-              name="(shop)"
-              options={{ headerShown: false, title: "Shop" }}
-            />
-            <Stack.Screen
-              name="categories"
-              options={{ headerShown: false, title: "Categories" }}
-            />
-            <Stack.Screen
-              name="product"
-              options={{ headerShown: false, title: "Product" }}
-            />
-            <Stack.Screen
-              name="cart"
-              options={{ presentation: "modal", title: "Shopping Cart" }}
-            />
-            <Stack.Screen
-              name="auth"
-              options={{ headerShown: false, title: "Auth" }}
-            />
-          </Stack>
+          <NotificationProvider>
+            <Stack>
+              <Stack.Screen
+                name="(shop)"
+                options={{ headerShown: false, title: "Shop" }}
+              />
+              <Stack.Screen
+                name="categories"
+                options={{ headerShown: false, title: "Categories" }}
+              />
+              <Stack.Screen
+                name="product"
+                options={{ headerShown: false, title: "Product" }}
+              />
+              <Stack.Screen
+                name="cart"
+                options={{ presentation: "modal", title: "Shopping Cart" }}
+              />
+              <Stack.Screen
+                name="auth"
+                options={{ headerShown: false, title: "Auth" }}
+              />
+            </Stack>
+          </NotificationProvider>
         </QueryProvider>
       </AuthProvider>
     </ToastProvider>
